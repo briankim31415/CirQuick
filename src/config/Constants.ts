3d0aff552ad64abcdd7b4cab388bee48ff0313d7
Kushal Shah <kushalshah@flash.net>
2021-10-04 15:56:10 -0500

    oops delete backend code from main

--- src/config/Constants.ts
+++
@@ -1,6 +0,0 @@
-export default class Constants {
-	static readonly serviceName:string = "cirquick";
-    static readonly version:string = "0.0.0";
-	//TODO replace mongo string
-	static readonly mongoUrl:string="REPLACE"
-}
