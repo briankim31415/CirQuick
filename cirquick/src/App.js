74f39be2b8e95218e878a8b421212cc19247ac8d
Brian <briankim31415@gmail.com>
2021-10-18 14:39:23 -0500

    Create dataset download page

--- cirquick/src/App.js
+++ cirquick/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from './login';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
 import signUp from './signUp';
+import datasets from './datasets';
 import blank from './blank';
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
 
         <Switch>
           <Route path="/signUp" exact component = {signUp}></Route>
-          <Route path="/dataSet" exact component = {blank}></Route>
+          <Route path="/dataSet" exact component = {datasets}></Route>
+          <Route path="/blank" exact component = {blank}></Route>
           <Route path="/" exact component = {Login}></Route>
         </Switch>
         
