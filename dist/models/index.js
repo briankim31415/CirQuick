f3c7af114d1e76a9d284cd98b70053d060fbe8a6
Kushal Shah <kushalshah@flash.net>
2021-10-21 17:20:38 -0500

    added dist

---
+++ dist/models/index.js
@@ -0,0 +1,36 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./checkinout-checkin-body"), exports);
+__exportStar(require("./checkinout-checkout-body"), exports);
+__exportStar(require("./echo-body"), exports);
+__exportStar(require("./inline-response200"), exports);
+__exportStar(require("./inline-response2001"), exports);
+__exportStar(require("./inline-response2002"), exports);
+__exportStar(require("./inline-response2003"), exports);
+__exportStar(require("./inline-response2004"), exports);
+__exportStar(require("./inline-response2005"), exports);
+__exportStar(require("./inline-response2006"), exports);
+__exportStar(require("./partial-user-interface-projects-joined"), exports);
+__exportStar(require("./partial-user-interface-transactions"), exports);
+__exportStar(require("./partial-user-interface_"), exports);
+__exportStar(require("./project-add-user-body"), exports);
+__exportStar(require("./project-create-body"), exports);
+__exportStar(require("./project-interface"), exports);
+__exportStar(require("./project-interface-resources"), exports);
+__exportStar(require("./project-interface-transactions"), exports);
+__exportStar(require("./project-interface-users-checked-out"), exports);
+__exportStar(require("./reset-body"), exports);
+__exportStar(require("./resource-interface"), exports);
+__exportStar(require("./resource-interface-transactions"), exports);
+__exportStar(require("./signin-body"), exports);
+__exportStar(require("./signup-body"), exports);
---
+++ dist/models/partial-user-interface-projects-joined.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PartialUserInterfaceProjectsJoinedRoleEnum = void 0;
+/**
+    * @export
+    * @enum {string}
+    */
+var PartialUserInterfaceProjectsJoinedRoleEnum;
+(function (PartialUserInterfaceProjectsJoinedRoleEnum) {
+    PartialUserInterfaceProjectsJoinedRoleEnum["Admin"] = "admin";
+    PartialUserInterfaceProjectsJoinedRoleEnum["Member"] = "member";
+})(PartialUserInterfaceProjectsJoinedRoleEnum = exports.PartialUserInterfaceProjectsJoinedRoleEnum || (exports.PartialUserInterfaceProjectsJoinedRoleEnum = {}));
